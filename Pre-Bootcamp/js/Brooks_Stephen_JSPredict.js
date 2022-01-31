c3b37f8a32ec7153d6d474438921266e7f6e9b27
Stephen Brooks <codedev1980@gmail.com>
2022-01-31 15:12:41 -0600

    added js file

---
+++ Pre-Bootcamp/js/Brooks_Stephen_JSPredict.js
@@ -0,0 +1,25 @@
+// Predict 1:
+function myBirthYearFunc(){
+    console.log("I was born in " + 1980);
+}
+myBirthYearFunc();
+
+////////////////////////////////
+// Predict2
+function myBirthYearFunc(birthYearInput){
+        console.log("I was born in " + birthYearInput);
+    }
+
+myBirthYearFunc();
+
+////////////////////////////////
+// Predict 3
+function add(num1, num2){
+        console.log("Summing Numbers!");
+        console.log("num1 is: " + num1);
+        console.log("num2 is: " + num2);
+        var sum = num1 + num2;
+        console.log(sum);
+    }
+    
+add();    
